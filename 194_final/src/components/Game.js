a15e5a0087579a2f8454ee0781abc363c4243262
Kenny <45550338+kdao167@users.noreply.github.com>
2024-02-04 20:54:36 -0800

    added very basic guessing game pulling fiestore

--- 194_final/src/components/Game.js
+++ 194_final/src/components/Game.js
@@ -1,66 +1,61 @@
-// import React, { useEffect, useRef, useState } from "react";
-// import {
-//   query,
-//   collection,
-//   orderBy,
-//   onSnapshot,
-//   limit,
-//   getDocs,
-// } from "firebase/firestore";
-// import { db } from "../firebase";
-// import Message from "./Message";
-// import SendMessage from "./SendMessage";
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase'; // Adjust this import according to your Firebase config file's location
 
-// export default function Game() {
-//   // Set up
-//   const [validGuesses, setValidGuesses] = useState([]);
-//   const [guesses, setGuesses] = useState([]);
-//   const [correctGuess, setCorrectGuess] = useState(false);
-//   let user = null;
-//   let querySnapshot = null;
+function Game() {
+  const [randomUser, setRandomUser] = useState(null);
+  const [guess, setGuess] = useState('');
+  const [feedback, setFeedback] = useState('');
 
+  useEffect(() => {
+    const fetchRandomUser = async () => {
+      const querySnapshot = await getDocs(collection(db, "users"));
+      const users = querySnapshot.docs.map(doc => doc.data());
+      if (users.length > 0) {
+        const randomIndex = Math.floor(Math.random() * users.length);
+        setRandomUser(users[randomIndex]);
+      }
+    };
 
-//   // Get a random person from the database
-//   useEffect(() => {
-//     FetchRandomPerson();
-//   }, []);
+    fetchRandomUser();
+  }, []);
+  console.log(randomUser)
 
-//   const FetchRandomPerson = () => {
-//     querySnapshot = getDocs(query(collection(db, "users")));
-//     user = querySnapshot.then(r => user = r.docs[Math.floor(Math.random() * 5)].data());
-//     setGuesses(true)
-//   }
+  const handleGuessSubmit = (event) => {
+    event.preventDefault();
+    if (guess.toLowerCase() === randomUser['FirstName'].toLowerCase()) {
+      setFeedback('Correct! You guessed the right user.');
+    } else {
+      setFeedback('Incorrect guess. Try again!');
+    }
+  };
 
-//   // get a random person from database
-  
-//   console.log(user['FirstName']);
+  const handleGuessChange = (event) => {
+    setGuess(event.target.value);
+  };
 
-//   // Populate valid guesses, guesses made, etc.
+  return (
+    <div>
+      <h2>Guess the User's Name</h2>
+      {randomUser ? (
+        <>
+          <p>Can you guess the name of the user?</p>
+          <form onSubmit={handleGuessSubmit}>
+            <input
+              type="text"
+              value={guess}
+              onChange={handleGuessChange}
+              placeholder="Enter your guess"
+            />
+            <button type="submit">Guess</button>
+          </form>
+          {feedback && <p>{feedback}</p>}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
 
-//   // Now you have an array of user data in validGuesses
-//   // You can iterate through it to get the first names
-
-// })
-
-
-
-
-
-
-
-
-
-//   return (<p>hello</p>)
-//   // return (
-//   //     <main className="chat-box">
-//   //         <div className="messages-wrapper">
-//   //             {messages?.map((message) => (
-//   //                 <Message key={message.id} message={message} />
-//   //             ))}
-//   //         </div>
-//   //         {/* when a new message enters the chat, the screen scrolls down to the scroll div */}
-//   //         <span ref={scroll}></span>
-//   //         <SendMessage scroll={scroll} />
-//   //     </main>
-//   // );
-// };
+export default Game;
