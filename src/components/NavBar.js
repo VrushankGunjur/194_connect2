072b0102288642ab85a8f6c002f11c650c9bef6f
Epicsurya <96981697+srijaladi@users.noreply.github.com>
2024-03-13 12:26:13 -0700

    Updated info page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -211,7 +211,6 @@ const NavBar = ({ currUserGroup, setCurrUserGroup, isNewUser, updateProfileTrue,
               <li>🔍 <strong>Arrows</strong> indicate the direction your match is compared to your guess.</li>
               <li><strong>Colors</strong> beyond red and green show how close your match is to your guess.</li>
             </ul>
-            <p>Matches where both users are successful will appear on the leaderboard!</p>
             <p>Out of guesses? You won't get your match's responses, but will get their contact!</p>
             <button onClick={togglePopup}>GO!</button>
           </div>
